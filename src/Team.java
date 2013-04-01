a235acc757d6de0db092717cd49457fd297f4a63
Matteo Bellistri <mbelli@MB.local>
2013-04-01 01:15:50 -0400

    Created Team class and added a couple of methods

--- src/Team.java
+++ src/Team.java
@@ -1,4 +1,28 @@
+import java.util.*;
 
 public class Team {
-
+	//List of players on the team
+	private List<Player> players;
+	
+	//Initialize the list that will hold the players
+	public Team () {
+		players = new ArrayList<Player>();
+	}
+	
+	//Add player to the team
+	public void addPlayer(Player p) {
+		players.add(p);
+	}
+	
+	//Remove player from team
+	public void removePlayer(Player p){
+		players.remove(p);
+	}
+	
+	//Get the number of players on the team
+	public int teamSize() {
+		return players.size();
+	}
+	
+	
 }
